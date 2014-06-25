0400eccc247ff85e9003d86d5e67f3f95bda08b4
Johnny Chen <Johnny@Johnnys-MacBook-Pro.local>
2014-06-25 17:00:02 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -150,6 +150,7 @@ static const int GRID_COLUMNS = 10;
             if (currentCreature.livingNeighbors == 3) {
                 currentCreature.isAlive = TRUE;
                 numAlive++;
+                NSLog(@"%d,%d",i,j);
             }
             else
                 currentCreature.isAlive = FALSE;
