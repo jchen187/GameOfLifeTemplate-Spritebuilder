21d939253d91432161fec1621983f5d6909a4c9a
Johnny Chen <Johnny@Johnnys-MacBook-Pro.local>
2014-06-25 12:01:18 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -59,7 +59,7 @@ static const int GRID_COLUMNS = 10;
             // make creatures visible to test this method, remove this once we know we have filled the grid properly
             creature.isAlive = YES;
             
-            x+=_cellWidth;
+            x += _cellWidth;
         }
         
         y += _cellHeight;
