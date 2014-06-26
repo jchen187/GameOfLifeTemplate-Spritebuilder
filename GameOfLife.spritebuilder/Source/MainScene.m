7997f2c37310dbf164031df6ca5d1fd19838efca
Johnny Chen <Johnny@Johnnys-MacBook-Pro.local>
2014-06-26 16:20:30 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -29,7 +29,7 @@
 - (void)play
 {
     //this tells the game to call a method called 'step' every half second
-    [self schedule:@selector(step) interval:0.5f];
+    [self schedule:@selector(step) interval:0.2f];
 }
 
 - (void)pause
